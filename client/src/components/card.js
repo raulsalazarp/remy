86a80b14d1a22e8fef632ab3ae8528690dfc8dbd
Lauren Newman <laurnewman1@gmail.com>
2023-03-08 13:41:52 -0500

    recipe card templates

---
+++ client/src/components/card.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import {Box, Typography, Rating} from '@mui/material';
+import { string, shape, number, bool } from "prop-types";
+import {styled} from '@mui/material/styles';
+import TimeIcon from '@mui/icons-material/AccessTimeRounded';
+import FireIcon from '@mui/icons-material/LocalFireDepartmentOutlined';
+import StarIcon from '@mui/icons-material/StarBorderRounded';
+import { useNavigate } from "react-router-dom";
+
+const StyledRating = styled((props) => (
+	<Rating size="small" readOnly {...props} />
+  )) (({ theme }) => ({
+	'& .MuiRating-iconFilled': {
+		color: theme.palette.primary.main,
+	  }
+  }));
+
+export default function RecipeCard({ recipe }) {
+
+    const navigate = useNavigate();
+
+    return (
+        <Box>
+            <img src="cookies.png" width="100%" height={125} style={{borderRadius: 7}}/>
+            <Box sx={{display: "flex", justifyContent: "space-between", alignItems: "center"}}>
+                <Typography fontSize={14} sx={{marginTop: "2px"}}>Recipe name</Typography>
+                <Box sx={{display: "flex", alignItems: "center"}}>
+                    <StyledRating value={5}/>
+                    <Typography color="secondary" fontSize={10}>(19)</Typography>
+                </Box>
+            </Box>
+            <Box sx={{display: "flex", alignItems: "center", gap: "6px", marginTop: "3px", marginBottom: "3px"}}>
+                <Box sx={{display: "flex", alignItems: "center", gap: "2px"}}>
+                    <TimeIcon color="secondary" fontSize="small" sx={{height: "18px"}}/>
+                    <Typography fontSize={11} color="secondary.dark">Time</Typography>
+                </Box>
+                <Box sx={{display: "flex", alignItems: "center", gap: "2px"}}>
+                    <FireIcon color="secondary" fontSize="small" sx={{height: "16px"}}/>
+                    <Typography fontSize={11} color="secondary.dark">Calories</Typography>
+                </Box>
+                <Box sx={{display: "flex", alignItems: "center", gap: "2px"}}>
+                    <StarIcon color="secondary" fontSize="small" sx={{height: "16px"}}/>
+                    <Typography fontSize={11} color="secondary.dark">Rating</Typography>
+                </Box>
+            </Box>
+            <Typography fontSize={11} color="secondary.dark">description description description description description descrption</Typography>
+        </Box>
+    )
+}
--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -4,6 +4,7 @@ import MuiBox from '@mui/material/Box';
 import {styled} from '@mui/material/styles';
 import Filter from "../components/filter";
 import Navbar from '../components/navbar';
+import RecipeCard from '../components/card';
 
 const Main = styled((props) => (
 	<MuiBox component="main" {...props} />
@@ -14,6 +15,7 @@ const Main = styled((props) => (
 export default function Home() {
 
     const [sortby, setSortby] = useState("Reviews");
+    const dummyRecipes = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
 
     return (
         <>
@@ -24,23 +26,27 @@ export default function Home() {
                     <Grid item xs={12}><Toolbar/></Grid>
                     <Grid item xs={12} sx={{padding: 2}}>
                         <Grid container spacing={3}>
-                        <Grid item xs={12}>
-                            <Box sx={{display: "flex", justifyContent: "flex-end", width: "100%"}}>
-                                <FormControl size="small">
-                                    <InputLabel>Sort By</InputLabel>
-                                    <Select
-                                        value={sortby}
-                                        label="Sort By"
-                                        onChange={(e) => setSortby(e.target.value)}
-                                    >
-                                        <MenuItem value={"Reviews"}>Reviews</MenuItem>
-                                        <MenuItem value={"Newest"}>Newest</MenuItem>
-                                        <MenuItem value={"Oldest"}>Oldest</MenuItem>
-                                    </Select>
-                                </FormControl>
-                            </Box>
-                        </Grid>
-                            <div style={{marginLeft: 100}}>TBD</div>
+                            <Grid item xs={12}>
+                                <Box sx={{display: "flex", justifyContent: "flex-end", width: "100%"}}>
+                                    <FormControl size="small">
+                                        <InputLabel>Sort By</InputLabel>
+                                        <Select
+                                            value={sortby}
+                                            label="Sort By"
+                                            onChange={(e) => setSortby(e.target.value)}
+                                        >
+                                            <MenuItem value={"Reviews"}>Reviews</MenuItem>
+                                            <MenuItem value={"Newest"}>Newest</MenuItem>
+                                            <MenuItem value={"Oldest"}>Oldest</MenuItem>
+                                        </Select>
+                                    </FormControl>
+                                </Box>
+                            </Grid>
+                            {dummyRecipes.length > 0 ?
+                            dummyRecipes.map(d => <Grid item xs={4}><RecipeCard /></Grid>)
+                            : <Typography variant="h5" sx={{minHeight: 445, marginLeft: 10, marginTop: 5}}>
+                                No results that match your search
+                                </Typography>}
                         </Grid>
                     </Grid>
                 </Grid>
